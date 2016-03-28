ffb817951b855a20fbc47f53c7ebfc807325a58f
Haider Malik <haidermalik5044@gmail.com>
2016-03-28 21:17:53 +0500

    Add Posts collection

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
     '$scope',
     function($scope) {
-        $scope.test = 'Hello World';
+
+        $scope.posts = [
+            { title: 'post1', upvotes: 5 },
+            { title: 'post2', upvotes: 15 },
+            { title: 'post3', upvotes: 9 },
+            { title: 'post4', upvotes: 4 },
+            { title: 'post5', upvotes: 7 }
+        ]
     }
 ])
